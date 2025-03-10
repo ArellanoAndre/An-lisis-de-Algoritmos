8ffa8b728bee5ae6ecc8b60990fd4a059467aa19
ArellanoAndre <isaac.arellano229842@potros.itson.edu.mx>
2025-03-09 18:14:02 -0700

    Implementacion de codigo del algoritmo Bubble sort y clase de prueba en metodo Main

---
+++ AA_UC2_229842/src/PruebasRun/Main.java
@@ -0,0 +1,39 @@
+package PruebasRun;
+
+import static Ordenamiento.BubbleSort.bubbleSort;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+/**
+ *
+ * @author Arell
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         int[] arreglo = {40,30,20,10,50}; // Arreglo de prueba
+
+       
+        ImprimirArreglo("antes de acomodo",arreglo); 
+        bubbleSort(arreglo); // Llamada al método de ordenamiento
+        ImprimirArreglo("despues de acomodo",arreglo); 
+        
+        }
+    
+    public static void ImprimirArreglo(String s,int arreglo[]){
+    System.out.println("\n\nArreglo " + s + ":");
+        for (int i = 0; i < arreglo.length; i++) {
+            System.out.print(arreglo[i] + " ");
+    }
+
+    }
+    
+    }
+    
+
