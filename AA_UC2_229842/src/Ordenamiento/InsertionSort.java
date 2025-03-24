0e87953eb3a06abe19d217db8e257bf32bacb252
ArellanoAndre <isaac.arellano229842@potros.itson.edu.mx>
2025-03-24 00:22:28 -0700

    Implementación del metodo InsertionSort junto con su codificacion necesaria para su ejecucion y prueba en el main

--- AA_UC2_229842/src/Ordenamiento/InsertionSort.java
+++ AA_UC2_229842/src/Ordenamiento/InsertionSort.java
@@ -9,5 +9,22 @@ package Ordenamiento;
  * @author Arell
  */
 public class InsertionSort {
-    
+    public static void insertionSort(int[] arr) {
+        int n = arr.length; // Obtiene el tamaño del arreglo
+
+        // Recorremos el arreglo desde la segunda posición (índice 1)
+        for (int i = 1; i < n; i++) {
+            int key = arr[i]; // Elemento actual que queremos insertar en su posición correcta
+            int j = i - 1; // Índice del elemento anterior en la parte ordenada
+
+            // Desplazamos los elementos mayores que "key" una posición hacia la derecha
+            while (j >= 0 && arr[j] > key) {
+                arr[j + 1] = arr[j]; // Mueve el elemento a la derecha
+                j--; // Decrementamos el índice para seguir comparando hacia la izquierda
+            }
+
+            // Insertamos "key" en la posición correcta
+            arr[j + 1] = key;
+        }
+    }
 }
